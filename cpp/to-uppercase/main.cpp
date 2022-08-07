d4b8e3a07a0dc269dc5e612541a2ae49a3c518c7
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-07 17:53:16 -0300

    add to uppercase program

---
+++ cpp/to-uppercase/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// ASCII of 'a' minus ASCII of 'A'
+const int CAPITALIZATION_ASCII_OFFSET = 97 - 65;
+
+char to_uppercase(char input) {
+  return input - CAPITALIZATION_ASCII_OFFSET;
+}
+
+int main() {
+  char input;
+  char result;
+
+  cout << "Please enter a lowercased character:\n";
+  cin >> input;
+
+  result = to_uppercase(input);
+
+  cout << "Uppercased '" << input << "' is '" << result << "'\n";
+
+  return 0;
+}
