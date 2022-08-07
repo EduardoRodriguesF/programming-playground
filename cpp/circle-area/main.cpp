fb6e6c430b622065865a99f899af7ddabb951ec6
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-07 17:16:41 -0300

    area of circle method

---
+++ cpp/circle-area/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  const double PI = atan(1) * 4;
+  double area;
+  double radius;
+
+  cout << "Please enter the radius of the circle:" << endl; 
+  cin >> radius;
+
+  area = PI * pow(radius, 2);
+
+  cout << "The area of the circle is " << area;
+
+  return 0;
+}
