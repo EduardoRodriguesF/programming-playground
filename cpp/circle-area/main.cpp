6cf3dba9f6a1f1c8a2bba120d939b9a6f3639f16
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-07 17:20:45 -0300

    use M_PI from cmath instead of our solution

--- cpp/circle-area/main.cpp
+++ cpp/circle-area/main.cpp
@@ -2,15 +2,13 @@
 #include <cmath>
 using namespace std;
 
-const double PI = atan(1) * 4;
-
 int main() {
   double area, radius;
 
   cout << "Please enter the radius of the circle:" << endl; 
   cin >> radius;
 
-  area = PI * pow(radius, 2);
+  area = M_PI * pow(radius, 2);
 
   cout << "The area of the circle is " << area;
 
