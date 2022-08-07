104e63b9c416ecd137e7c6632f2bf68f8b060c6a
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-08-07 16:53:07 -0300

    different return types for each kind of result

--- cpp/traveled-days/main.cpp
+++ cpp/traveled-days/main.cpp
@@ -11,5 +11,16 @@ int main() {
   int full_weeks = traveled_days / WEEK_DAYS;
   int additional_days = traveled_days % WEEK_DAYS;
 
+  if (full_weeks == 0) {
+    cout << "You've traveled only for " << additional_days << " days!";
+    return 1;
+  }
+
+  if (additional_days == 0) {
+    cout << "You've traveled for exactly " << full_weeks << " weeks";
+    return 0;
+  } 
+
   cout << "You've traveled for " << full_weeks << " weeks and " << additional_days << " additional days.";
+  return 0;
 }
