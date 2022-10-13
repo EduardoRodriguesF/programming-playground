5e8eebdaa3ff89072b093a9b2625734cf1fa787f
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-13 08:53:55 -0300

    refactor to extract functions

--- rust/file-handle/src/main.rs
+++ rust/file-handle/src/main.rs
@@ -1,19 +1,27 @@
 use std::fs::File;
 use std::io::ErrorKind;
 
-fn main() {
-    let greeting_file = match File::open("hello.txt") {
+fn create_file(filename: &str) -> File {
+    match File::create(filename) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem creating the file: {:?}", error),
+    }
+}
+
+fn read_or_create(filename: &str) -> File {
+    match File::open(filename) {
         Ok(file) => file,
         Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(new_file) => new_file,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            }
+            ErrorKind::NotFound => create_file(filename),
             other_error => {
                 panic!("Problem opening the file: {:?}", other_error)
             }
         }
-    };
+    }
+}
+
+fn main() {
+    let greeting_file = read_or_create("hello.txt");
 
     println!("{:?}", greeting_file);
 }
