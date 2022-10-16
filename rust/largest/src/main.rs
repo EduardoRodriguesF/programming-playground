83ea7b029be941d5a392369f8be41af58f834eb3
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-16 09:36:41 -0300

    refactor: change largest to use where syntax

--- rust/largest/src/main.rs
+++ rust/largest/src/main.rs
@@ -1,4 +1,5 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
+// fn largest<T: PartialOrd>(list: &[T]) -> &T { ... also works
+fn largest<T>(list: &[T]) -> &T where T: PartialOrd {
     let mut result = &list[0];
 
     for item in list {
