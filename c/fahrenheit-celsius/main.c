cb8eeb9c43bc616927b8287837655d2e5b676010
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-11-18 19:20:39 -0300

    [c] complete exercise 1-3

--- c/fahrenheit-celsius/main.c
+++ c/fahrenheit-celsius/main.c
@@ -4,14 +4,16 @@
 #define LOWER_LIMIT 0
 #define STEP 20
 
-
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
+
+    // Heading
+    printf("%3c\t%6c\n", 'F', 'C');
 
     fahr = LOWER_LIMIT;
     while (fahr <= UPPER_LIMIT) {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr += STEP;
     }
 }
